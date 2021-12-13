af9160d5eecab8863e18d5e2481254a5be14e86f
Peter <github@peter.dev.br>
2021-12-13 17:00:27 -0300

    WIP: add ThreadCard Component

---
+++ src/components/thread-participants-card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { User } from "types";
+
+type ParticipantsProps = {
+  participants: User[];
+};
+
+export default function ThreadParticipantsCard({
+  participants,
+}: ParticipantsProps) {
+  return <div>participants</div>;
+}
