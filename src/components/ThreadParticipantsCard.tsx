375b132322f4dc14a1d78bc9968fe7920013cddd
Peter <github@peter.dev.br>
2021-12-17 10:43:06 -0300

    feat: list thread's participants thumbnails

--- src/components/ThreadParticipantsCard.tsx
+++ src/components/ThreadParticipantsCard.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import { User } from "types";
+import Thumbnail, { ThumbnailSize } from "./Thumbnail";
+import styles from "./ThreadParticipantsCard.module.scss";
+import { partition } from "utils";
 
 type ParticipantsProps = {
   participants: User[];
+  size?: number;
 };
 
 export default function ThreadParticipantsCard({
   participants,
+  size = 2,
 }: ParticipantsProps) {
-  return <div>participants</div>;
+  const [users, others] = partition<User>(
+    participants,
+    ([first]) => first.length < size
+  );
+
+  return (
+    <div className={styles.stack}>
+      {users.map(user => {
+        return (
+          <Thumbnail
+            key={user.id}
+            user={user}
+            size={ThumbnailSize.TINY}
+            framed
+          />
+        );
+      })}
+
+      {others.length > 0 && (
+        <Thumbnail size={ThumbnailSize.TINY} framed>
+          +{others.length}
+        </Thumbnail>
+      )}
+    </div>
+  );
 }
