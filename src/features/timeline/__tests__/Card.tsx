d4b63c9921ba3880a79337ff187995b87ea2a8c9
Peter <github@peter.dev.br>
2021-12-22 21:13:30 -0300

    test: happy path for timeline Card

--- src/features/timeline/__tests__/Card.tsx
+++ src/features/timeline/__tests__/Card.tsx
@@ -1,13 +1,17 @@
 import { render } from "@testing-library/react";
-import { ThreadFactory } from "test/factories";
+import { PostFactory, ThreadFactory } from "test/factories";
 import Card from "../Card";
 
 describe("Timeline Card", () => {
   test("render thread information", () => {
-    const thread = ThreadFactory.build();
-
+    const description = "nice message";
+    const post = PostFactory.build({ message: description });
+    const thread = ThreadFactory.build({}, { associations: { post } });
     const { container } = render(<Card {...thread} />);
 
     expect(container).toHaveTextContent(thread.title);
+    expect(container).toHaveTextContent(description);
+    expect(container).toHaveTextContent(thread.title);
+    expect(container).toHaveTextContent(post.author.username);
   });
 });
