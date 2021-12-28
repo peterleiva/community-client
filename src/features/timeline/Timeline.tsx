3cdc8f55fba684dfc6b8bc960e06a363d9b72e01
Peter <github@peter.dev.br>
2021-12-28 00:21:23 -0300

    refactor(timeline): aggregate logic to load page

--- src/features/timeline/Timeline.tsx
+++ src/features/timeline/Timeline.tsx
@@ -1,13 +1,22 @@
-import type { Cursor, Thread } from "types";
+import type { Thread } from "types";
+import { Button, CaughUp } from "components";
 import EmptyState from "./EmptyState";
 import Card from "./Card";
 import styles from "./Timeline.module.scss";
 
 type TimelineProps = {
   threads?: Thread[];
+  caughUp: boolean;
+  loading: boolean;
+  onNextPage: () => void;
 };
 
-export default function Timeline({ threads = [] }: TimelineProps): JSX.Element {
+export default function Timeline({
+  threads = [],
+  onNextPage,
+  caughUp = false,
+  loading = false,
+}: TimelineProps): JSX.Element {
   if (threads.length == 0) {
     return <EmptyState />;
   }
@@ -17,6 +26,21 @@ export default function Timeline({ threads = [] }: TimelineProps): JSX.Element {
       {threads.map(thread => (
         <Card key={thread.id} {...thread} />
       ))}
+      <div>
+        {loading && <small>loading...</small>}
+        {caughUp && (
+          <CaughUp lastActivity={threads[threads.length - 1].activity} />
+        )}
+        {!caughUp && (
+          <Button
+            renderContainer={props => (
+              <button {...props} onClick={onNextPage}>
+                Load more
+              </button>
+            )}
+          />
+        )}
+      </div>
     </div>
   );
 }
