743dd5f25dd96020011ec800450bed83483b0dfa
Peter <github@peter.dev.br>
2021-12-27 23:50:12 -0300

    feat: add CaughUp component

---
+++ src/components/CaughUp.tsx
@@ -0,0 +1,23 @@
+import { DateTime } from "luxon";
+import RelativeTime from "./RelativeTime";
+import { BsCheckCircle } from "react-icons/bs";
+import styles from "./CaughUp.module.scss";
+
+type CaughUpProps = {
+  title?: string;
+  lastActivity: DateTime;
+};
+
+export default function CaughUp({ title, lastActivity }: CaughUpProps) {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>{title && "You are caugh up"}</h1>
+        <p>
+          <RelativeTime time={lastActivity} />
+        </p>
+      </div>
+      <BsCheckCircle className={styles.icon} />
+    </div>
+  );
+}
