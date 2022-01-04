e142bf43da9907527fd0d1c1be2548f9f19041af
Peter <github@peter.dev.br>
2022-01-04 16:35:55 -0300

    test(timeline): fix Timeline component test

--- src/features/timeline/__tests__/Timeline.tsx
+++ src/features/timeline/__tests__/Timeline.tsx
@@ -1,6 +1,6 @@
-import Timeline from "../Timeline";
 import { render } from "@testing-library/react";
 import { ThreadFactory } from "test/factories";
+import Timeline from "../Timeline";
 
 describe("Timeline", () => {
   const threads = ThreadFactory.buildList(2);
@@ -10,18 +10,6 @@ describe("Timeline", () => {
 
     expect(container).toHaveTextContent(threads[0].title);
     expect(container).toHaveTextContent(threads[1].title);
-  });
-
-  test("show caughUp component when caughUp is true", () => {
-    const { container } = render(<Timeline threads={threads} caughUp={true} />);
-
-    expect(container).toHaveTextContent(/caugh up/i);
-  });
-
-  test("render loading information", () => {
-    const { container } = render(<Timeline loading={true} threads={threads} />);
-
-    expect(container).toHaveTextContent(/loading/i);
   });
 
   test.todo("render empty state when threads is empty");
