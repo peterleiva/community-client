ae4e2d275dcc804201dd965ae1ffa4f5dd70daf9
Peter <github@peter.dev.br>
2022-01-04 11:05:29 -0300

    refactor: add className prop in CaughUp component

--- src/components/CaughUp.tsx
+++ src/components/CaughUp.tsx
@@ -1,23 +1,31 @@
 import { DateTime } from "luxon";
 import RelativeTime from "./RelativeTime";
-import { BsCheckCircle } from "react-icons/bs";
+import { BsCheckCircle as DoneIcon } from "react-icons/bs";
 import styles from "./CaughUp.module.scss";
+import { applyClasses } from "lib";
 
 type CaughUpProps = {
   title?: string;
   lastActivity: DateTime;
+  className?: string;
 };
 
-export default function CaughUp({ title, lastActivity }: CaughUpProps) {
+export default function CaughUp({
+  title,
+  lastActivity,
+  className = "",
+}: CaughUpProps) {
+  const classes = applyClasses(styles.container, className);
+
   return (
-    <div className={styles.container}>
+    <div className={classes}>
       <div>
         <h1>{title ?? "You are caugh up"}</h1>
         <p>
           <RelativeTime time={lastActivity} />
         </p>
       </div>
-      <BsCheckCircle className={styles.icon} />
+      <DoneIcon className={styles.icon} />
     </div>
   );
 }
