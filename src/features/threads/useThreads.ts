1001663937ec1836e7294c50eeaf5edc83898947
Peter <github@peter.dev.br>
2022-01-07 20:06:29 -0300

    fix: useThreads load next page

--- src/features/threads/useThreads.ts
+++ src/features/threads/useThreads.ts
@@ -4,25 +4,35 @@ import { GET_THREADS, PageVariables, GetThreadsQuery } from "./graphql";
 import { usePageLoader } from "hooks";
 import { threadsConnectionMapper } from "./mapper";
 import { ThreadConnection } from ".";
+import { useCallback, useEffect, useState } from "react";
+import { Cursor, PageInfo } from "types";
 
 type Options = {
   sample?: number;
   threads?: ThreadConnection;
 };
 
-export default function useThreads({ sample, threads }: Options = {}) {
-  const { cursor, nextPage, ...pageLoader } = usePageLoader<Thread>({
-    cursor: threads?.pageInfo?.endCursor,
-    data: threadsConnectionMapper(threads),
-  });
+export default function useThreads({
+  sample,
+  threads: initialThreads,
+}: Options = {}) {
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [threads, setThreads] = useState<Thread[]>(
+    threadsConnectionMapper(initialThreads)
+  );
 
+  const [page, setPage] = useState<PageInfo | undefined>(
+    initialThreads?.pageInfo
+  );
+
+  const [batches, setBatches] = useState<ThreadConnection[]>([]);
   const { loading, data, error } = useQuery<GetThreadsQuery, PageVariables>(
     GET_THREADS,
     {
       fetchPolicy: "no-cache",
       variables: {
         page: {
-          after: cursor,
+          after: page?.endCursor,
           first: sample,
         },
       },
@@ -31,12 +41,36 @@ export default function useThreads({ sample, threads }: Options = {}) {
 
   const total = data?.threads.total ?? -Infinity;
 
+  useEffect(() => {
+    const threads = data?.threads ?? [];
+
+    setBatches(prev => prev.concat(threads));
+  }, [data, setBatches]);
+
+  const next = useCallback(() => {
+    const nextBatch = batches.shift();
+
+    if (nextBatch && hasNextPage) {
+      const threads = threadsConnectionMapper(nextBatch);
+
+      setThreads(prev => prev.concat(threads));
+      setPage(nextBatch.pageInfo);
+      setHasNextPage(nextBatch.pageInfo.hasNextPage);
+      setBatches(prev => {
+        prev.shift();
+        return prev;
+      });
+    }
+  }, [batches, setThreads, setPage, setHasNextPage, hasNextPage]);
+
   return {
-    ...pageLoader,
     loading,
     total,
+    data: threads,
+    caughUp: !hasNextPage,
+    hasNextPage,
+    page: data,
     error,
-    nextPage: () =>
-      nextPage(threadsConnectionMapper(data?.threads), data?.threads?.pageInfo),
+    next,
   };
 }
