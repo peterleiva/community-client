82c53f588b3e3272f264a5680517e076feab3536
Peter <github@peter.dev.br>
2022-03-18 12:15:57 -0300

    test: add play story function to password input

--- src/components/forms/__stories__/Password.tsx
+++ src/components/forms/__stories__/Password.tsx
@@ -1,4 +1,5 @@
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
+import { screen, userEvent } from "@storybook/testing-library";
 import Password from "../Password";
 
 type PasswordComponent = typeof Password;
@@ -15,6 +16,16 @@ const Template: ComponentStory<PasswordComponent> = args => (
 export const Decorated = Template.bind({});
 Decorated.args = {
   placeholder: "password",
+};
+
+Decorated.play = async () => {
+  const input = screen.getByPlaceholderText("password");
+
+  await userEvent.type(input, "secret", { delay: 100 });
+  userEvent.click(screen.getByRole("button"));
+
+  await userEvent.type(input, "_pass", { delay: 300 });
+  userEvent.click(screen.getByRole("button"));
 };
 
 export const Input = Template.bind({});
