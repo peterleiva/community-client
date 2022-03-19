365b3414afc4aa317cda737d38c92f76cfa53103
Peter <github@peter.dev.br>
2022-03-19 09:58:18 -0300

    chore: setup framer-motion

--- .storybook/main.js
+++ .storybook/main.js
@@ -27,6 +27,12 @@ module.exports = {
       path.resolve(__dirname, "../src"),
     ];
 
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+
     return config;
   },
 };
