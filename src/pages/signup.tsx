771e63be90e82c55f5950de7efcaf48171f5fcfc
Peter <github@peter.dev.br>
2022-03-23 15:05:42 -0300

    refactor: use form custom components in SignUp page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import { Label, TextInput, PasswordInput, Form } from "components/forms";
+import { Form, TextField } from "components/forms";
 import Button from "components/Button";
 import { useToast } from "components/toast";
 import { BsArrowRight as RightIcon } from "react-icons/bs";
@@ -8,10 +8,6 @@ import { type SubmitHandler } from "react-hook-form";
 type FormData = {
   email: string;
   password: string;
-};
-
-const Errors = ({ error }: { error?: string }) => {
-  return <p className="text-red-500">{error}</p>;
 };
 
 const SignUp: NextPage = () => {
@@ -29,41 +25,19 @@ const SignUp: NextPage = () => {
   return (
     <div className="w-3/4 mx-auto">
       <h1>Sign Up</h1>
-      <Form<FormData> onSubmit={submission}>
-        {({ register, formState: { errors } }) => (
-          <>
-            <div className="form-control">
-              <Label htmlFor="email" required>
-                Email
-              </Label>
-              <TextInput
-                id="email"
-                error={!!errors.email}
-                register={register("email", { required: "email is required" })}
-              />
-              <Errors error={errors.email?.message} />
-            </div>
-            <div className="form-control">
-              <Label htmlFor="password" required>
-                Password
-              </Label>
-              <PasswordInput
-                id="password"
-                error={!!errors.password}
-                autoComplete="current-password"
-                register={register("password", {
-                  required: "password is required",
-                })}
-              />
-              <Errors error={errors.password?.message} />
-            </div>
-            <div className="flex justify-end">
-              <Button type="submit" endIcon={<RightIcon />}>
-                Register
-              </Button>
-            </div>
-          </>
-        )}
+      <Form<FormData> onSubmit={submission} className="flex flex-col gap-4">
+        <TextField name="email" label="Email" required />
+        <TextField
+          name="password"
+          label="Password"
+          autoComplete="current-password"
+          required
+        />
+        <div className="flex justify-end">
+          <Button type="submit" endIcon={<RightIcon />}>
+            Register
+          </Button>
+        </div>
       </Form>
 
       <style jsx>{`
